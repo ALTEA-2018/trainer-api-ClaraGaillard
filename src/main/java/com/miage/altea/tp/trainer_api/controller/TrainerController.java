732443dafab85a9f3275bae117953f3a1ae2f71e
Gaillard Clara <gaillardc@b13p8.fil.univ-lille1.fr>
2019-03-11 12:04:44 +0100

    11 mars

--- src/main/java/com/miage/altea/tp/trainer_api/controller/TrainerController.java
+++ src/main/java/com/miage/altea/tp/trainer_api/controller/TrainerController.java
@@ -2,10 +2,8 @@ package com.miage.altea.tp.trainer_api.controller;
 
 import com.miage.altea.tp.trainer_api.bo.Trainer;
 import com.miage.altea.tp.trainer_api.service.TrainerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/trainers")
@@ -29,6 +27,11 @@ public class TrainerController {
         } else {
             return new Trainer();
         }
+    }
+
+    @PostMapping("/registerTrainer")
+    public Trainer registerNewTrainer(@RequestBody  Trainer trainer){
+        return trainerService.createTrainer(trainer);
     }
 
 }
